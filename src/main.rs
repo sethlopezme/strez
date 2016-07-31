c49b71644d900f07f4037f6c8efb50c01b5c298c
Seth Lopez <sethlopezme@users.noreply.github.com>
2016-07-31 12:41:53 -0400

    Add options/arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli/cli-en.yml");
+    let args = clap::App::from_yaml(yaml)
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .get_matches();
 }
