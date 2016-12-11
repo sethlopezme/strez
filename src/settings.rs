d3ecc7402b18f06f3c1c2afcddfee2343e3d95f6
Seth Lopez <sethlopezme@users.noreply.github.com>
2016-12-11 17:48:49 -0500

    Simplify the new method for settings

--- src/settings.rs
+++ src/settings.rs
@@ -2,58 +2,58 @@ use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
 
+use format::FormatKind;
 use platform::PlatformKind;
 use language::LanguageKind;
+use Result;
 
 extern crate clap;
 
 #[derive(Debug)]
-pub struct Settings<'a> {
+pub struct Settings {
     pub default_language: Option<LanguageKind>,
     pub force: bool,
-    pub format: &'a str,
+    pub format: FormatKind,
     pub infile: PathBuf,
     pub outdir: PathBuf,
     pub platform: Vec<PlatformKind>,
 }
 
-impl<'a> Settings<'a> {
+impl Settings {
     /*
      * Takes argument matches from clap and turns them into a Settings struct that is easier to use.
      */
-    pub fn from_clap(args: &'a clap::ArgMatches) -> Result<Settings<'a>, String> {
-        let settings = Settings {
+    pub fn from_clap<'a>(args: &'a clap::ArgMatches) -> Result<Settings> {
+        Ok(Settings {
             default_language: match args.value_of("default_language") {
                 Some(language_code) => Some(language_code.parse::<LanguageKind>()?),
                 None => None,
             },
             force: args.is_present("force"),
-            format: "csv",
-            infile: str_to_pathbuf(args.value_of("infile")
-                                       .ok_or(String::from("missing required infile argument"))?, true)?,
-            outdir: str_to_pathbuf(args.value_of("outdir")
-                                       .ok_or(String::from("missing required outdir argument"))?, false)?,
+            format: args.value_of("format")
+                        .unwrap_or("csv")
+                        .parse::<FormatKind>()?,
+            infile: str_to_pathbuf(args.value_of("infile").ok_or(String::from("missing required infile argument"))?, true)?,
+            outdir: str_to_pathbuf(args.value_of("outdir").ok_or(String::from("missing required outdir argument"))?, false)?,
             platform: args.values_of("platform")
-                          .map_or(Ok(vec![PlatformKind::Android, PlatformKind::iOS]), |platforms| {
+                          .map_or(Ok(vec![PlatformKind::Android, PlatformKind::Ios]), |platforms| {
                               platforms.map(|platform| platform.parse::<PlatformKind>())
-                                       .collect::<Result<Vec<PlatformKind>, String>>()
+                                       .collect::<Result<Vec<PlatformKind>>>()
                           })?
-        };
-
-        Ok(settings)
+        })
     }
 }
 
 /*
  * Converts a string into a PathBuf, ensuring that the path exists and is a file or directory.
  */
-fn str_to_pathbuf(path_str: &str, is_file: bool) -> Result<PathBuf, String> {
-    let pathbuf = fs::canonicalize(path_str).map_err(|e| format!("\"{}\" {}", path_str, e.description()))?;
+fn str_to_pathbuf(path_str: &str, is_file: bool) -> Result<PathBuf> {
+    let pathbuf = fs::canonicalize(path_str).map_err(|e| format!("{} \"{}\"", e.description(), path_str))?;
 
     if is_file && !pathbuf.is_file() {
-        Err(format!("\"{}\" is not a file", path_str))
+        Err(format!("invalid file \"{}\"", path_str))
     } else if !is_file && !pathbuf.is_dir() {
-        Err(format!("\"{}\" is not a directory", path_str))
+        Err(format!("invalid directory \"{}\"", path_str))
     } else {
         Ok(pathbuf)
     }
