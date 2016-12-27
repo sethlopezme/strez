fe94a9c41beece1a0be9850946c828cc3b0e4e79
Seth Lopez <sethlopezme@users.noreply.github.com>
2016-12-27 01:17:44 -0500

    Update Resource struct with final (for now) property definitions

--- src/resource.rs
+++ src/resource.rs
@@ -1,52 +1,18 @@
 use std::collections::HashMap;
 
-pub enum Translation<'a> {
-    Singular(&'a str),
-    Plural {
-        zero: &'a str,
-        one: &'a str,
-        two: &'a str,
-        few: &'a str,
-        many: &'a str,
-        other: &'a str,
-    }
+#[derive(Debug)]
+pub struct Resource {
+    pub description: Option<String>,
+    pub plural: bool,
+    pub translations: HashMap<String, Translation>,
 }
 
-pub struct Resource<'a> {
-    name: &'a str,
-    description: Option<&'a str>,
-    quantity: Option<&'a str>,
-}
-
-pub struct ResourceBuilder<'a> {
-    name: Option<&'a str>,
-    description: Option<&'a str>,
-    quantity: Option<&'a str>,
-    translations: HashMap<&'a str, Translation<'a>>,
-}
-
-impl<'a> ResourceBuilder<'a> {
-    fn new() -> ResourceBuilder<'a> {
-        ResourceBuilder {
-            name: None,
-            description: None,
-            quantity: None,
-            translations: HashMap::new(),
-        }
-    }
-
-    fn name(&mut self, name: &'a str) -> &mut Self {
-        self.name = Some(name);
-        self
-    }
-
-    fn description(&mut self, description: &'a str) -> &mut Self {
-        self.description = Some(description);
-        self
-    }
-
-    fn quantity(&mut self, quantity: &'a str) -> &mut Self {
-        self.quantity = Some(quantity);
-        self
-    }
+#[derive(Debug, Default)]
+pub struct Translation {
+    pub zero: Option<String>,
+    pub one: Option<String>,
+    pub two: Option<String>,
+    pub few: Option<String>,
+    pub many: Option<String>,
+    pub other: Option<String>,
 }
